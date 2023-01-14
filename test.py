4138307f3d21ca38f19d599898c8b56b18bd9a1f
Raehyeon Jeong <raehy19@gmail.com>
2023-01-14 20:57:57 +0900

    refactor : delete unnecessary files

--- test.py
+++ test.py
@@ -5,12 +5,10 @@ if __name__ == "__main__":
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
     # Train the model
-    train_results = model.train(data="data.yaml", epochs=100)
-    print(train_results)
+    train_results = model.train(data="data.yaml", epochs=10)
 
     # Validate the model
     val_results = model.val()  # no arguments needed, dataset and settings remembered
-    print(val_results)
 
     # Predict with the model
     # predict_results = model()  # predict on an image
